efe6d75a38d677ee1441376cf3676f13835fafb6
marcela <marcela.vilasboas@gmail.com>
2021-04-28 10:50:46 +0200

    delete projects

--- Food/client/src/store/modules/users/getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-    isLoggedIn: state => !!state.token,
-    getUser: state => state.user.username,
-    getUserId: state => state.user._id, 
-    getAllFavorites: state => state.recipes,
-    isFavoriteRecipe: state => state.recipes.isFavorite
-}
